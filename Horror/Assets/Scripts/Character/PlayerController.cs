c5673165e6d340dfa11721a136661f2962f991c9
RavshanRashavelly <56175043+RavshanRashavelly@users.noreply.github.com>
2022-04-22 18:45:55 +0300

    map

---
+++ Horror/Assets/Scripts/Character/PlayerController.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody), typeof(CapsuleCollider))]
+
+public class PlayerController : MonoBehaviour
+{
+
+    [SerializeField] private Rigidbody rb;
+    [SerializeField] private FixedJoystick joystick;
+
+    [SerializeField] private float speed;
+
+  
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector3(joystick.Horizontal * speed, rb.velocity.y, joystick.Vertical + speed);
+    }
+}
